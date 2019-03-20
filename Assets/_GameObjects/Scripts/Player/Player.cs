679d31c6152e57370193195e303ad4f2527ed7ea
Antonio Sanz Pans <aspans@gmail.com>
2019-03-20 21:06:04 +0100

    HUD + Anim
    Manage Heal
    Animations
    Healbar created

--- Assets/_GameObjects/Scripts/Player/Player.cs
+++ Assets/_GameObjects/Scripts/Player/Player.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animator animator;
     private AudioSource audiosource;
+    [SerializeField] Slider sliderhp;
 
-    private float x, z;
+    private float x, y, z;
+    private const int MAX_HP = 100;
+    private int currentHp;
 
     public enum State { Idle, Walking, Running, Jumping }
     public State estado = State.Idle;
@@ -17,31 +22,72 @@ public class Player : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        currentHp = 100;
+    }
 
     private void Update()
     {
-        //Input.mousePosition
+        if (Input.GetKeyDown(KeyCode.W))
+        {
 
+        }
+
+        ManageAtack();
+        DebugAnimations();
+    }
+
+    private void ManageAtack()
+    {
         if (Input.GetButtonDown(texts.FIRE_1))
         {
-            Debug.Log("FIRE1");
+            Debug.Log("FIRE1 RANGE");
             animator.SetTrigger(texts.ANIM_SHOOT);
 
         }
         if (Input.GetButtonDown(texts.FIRE_2))
         {
-            Debug.Log("FIRE2");
+            Debug.Log("FIRE2 MELEE");
             animator.SetTrigger(texts.ANIM_ATACK);
-
         }
     }
 
-    private void TakeDamage()
+    private void DebugAnimations()
     {
-        animator.SetTrigger(texts.ANIM_TAKEDAMAGE);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TakeDamage(10);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Die();
+        }
     }
-    private void Death()
-    {
 
+    private void TakeDamage(int _damage)
+    {
+        Debug.Log("TakeDamage.");
+        Debug.Log("HP: " + currentHp);
+
+        currentHp = currentHp - _damage;
+        sliderhp.value = currentHp;
+        if (currentHp > 0)
+        {
+            animator.SetTrigger(texts.ANIM_TAKEDAMAGE);
+        }
+        else
+        {
+            Die();
+        }
     }
+
+    private void Die()
+    {
+        Debug.Log("Die");
+        animator.SetTrigger(texts.ANIM_DIE);
+    }
+
+    //Cuando el player se mueve, el mago mira hacia donde se está moviendo
+    //Cuando el player hace click del ratón, el mago mira hacia donde ha hecho click el jugador y hace la habilidad asignada hacia ahí y vuelve a mirar ¿hacia donde lo estaba haciendo o se queda mirando a donde ataca?
 }
--- Assets/_GameObjects/Scripts/texts.cs
+++ Assets/_GameObjects/Scripts/texts.cs
@@ -12,10 +12,15 @@ public class texts
     //Buttons
     public const string FIRE_1 = "Fire1";
     public const string FIRE_2 = "Fire2";
+    public const string MOVE_UP = "";
+    public const string MOVE_BOT = "";
+    public const string MOVE_LEFT = "";
+    public const string MOVE_RIGHT = "";
     //Animations
     public const string ANIM_ATACK = "Atack";
     public const string ANIM_SHOOT = "Shoot";
     public const string ANIM_TAKEDAMAGE = "TakeDamage";
+    public const string ANIM_DIE = "Die";
 }
 
 
